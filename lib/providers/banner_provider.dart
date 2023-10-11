d19255b04b0ae9ce8e5fa1ee5c452459878c140c
Rana Nilam <rananilam84@gmail.com>
2023-10-11 17:00:07 +0100

    Nymble_Project_Nilam

--- lib/providers/banner_provider.dart
+++ lib/providers/banner_provider.dart
@@ -9,45 +9,16 @@ import '../model/main_banner_model.dart';
 
 
 class BannerProvider extends ChangeNotifier {
-  double _value = 1.0;
-  int _currentPage = 0;
+
   PageController pageController = PageController();
 
   List<data_model> _data = [];
   main_banner_model? _model ;
-  List onBoardingData = [
-    {
-      "image": "assets/images/screen1.png",
-      "title": "Care your family",
-      "description": "The process can include education new"
-    },
-    {
-      "image": "assets/images/screen2.png",
-      "title": "Act ahead of time",
-      "description": "No holding back"
-    },
-    {
-      "image": "assets/images/screen3.png",
-      "title": "Go ahead.....!",
-      "description": "See the world...."
-    },
-  ];
 
-  double get value => _value;
 
-  int get currentPage => _currentPage;
   List<data_model> get data => _data;
   main_banner_model get model => _model!;
 
-  void changeValue(val) {
-    _value = val;
-    notifyListeners();
-  }
-
-  void onchanged(index) {
-    _currentPage = index;
-    notifyListeners();
-  }
 
   Future<List<data_model>> readJsonData() async {
     final jsonData =
